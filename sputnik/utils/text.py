08954bcf16fcb391aa45b4565f10d213748452bc
skar404 <skar404@gmail.com>
2019-05-02 17:42:54 +0300

    #2 test

--- sputnik/utils/text.py
+++ sputnik/utils/text.py
@@ -1,12 +1,12 @@
-from sputnik.models.post import PostModel
-
-
-def get_post_text(post: PostModel) -> str:
-    text = f"【{post.title}】" \
-        f"{post.description} " \
-        f"{post.short_link}"
-    return text
-
-
-def markdown_shielding(text: str) -> str:
-    return text.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
+# from sputnik.models.post import PostModel
+#
+#
+# def get_post_text(post: PostModel) -> str:
+#     text = f"【{post.title}】" \
+#         f"{post.description} " \
+#         f"{post.short_link}"
+#     return text
+#
+#
+# def markdown_shielding(text: str) -> str:
+#     return text.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
