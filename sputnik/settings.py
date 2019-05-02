29a769459ad6622bd1f2c73308b872beace96786
skar404 <skar404@gmail.com>
2019-05-02 16:10:34 +0300

    #2 send default value

--- sputnik/settings.py
+++ sputnik/settings.py
@@ -16,7 +16,7 @@ TIME_OUT: int = env.int('TIME_OUT', default=300)
 
 BOT_TOKEN: str = env.str('BOT_TOKEN', default=None)
 BOT_SECRET_URL: str = env.str('BOT_SECRET_URL', default='')
-BOT_WEB_HOOK: str = env.str('BOT_WEB_HOOK', default=None) + BOT_SECRET_URL
+BOT_WEB_HOOK: str = env.str('BOT_WEB_HOOK', default='') + BOT_SECRET_URL
 BOT_SECRET_CALLBACK: str = env.str('BOT_SECRET_CALLBACK', default='dev')
 
 RSS_FEED: str = env.str('RSS_FEED', default=None)
@@ -26,11 +26,11 @@ WEIBO_APP_KEY: str = env.str('WEIBO_APP_KEY', default=None)
 WEIBO_APP_SECRET: str = env.str('WEIBO_APP_SECRET', default=None)
 WEIBO_APP_HOST: str = env.str('WEIBO_APP_HOST', default=None)
 
-WEIBO_LOGIN: str = env.str('WEIBO_LOGIN')
-WEIBO_PASSWORD: str = env.str('WEIBO_PASSWORD')
+WEIBO_LOGIN: str = env.str('WEIBO_LOGIN', default=None)
+WEIBO_PASSWORD: str = env.str('WEIBO_PASSWORD', default=None)
 
-WEIBO_TEST_LOGIN: str = env.str('WEIBO_LOGIN')
-WEIBO_TEST_PASSWORD: str = env.str('WEIBO_PASSWORD')
+WEIBO_TEST_LOGIN: str = env.str('WEIBO_LOGIN', default=None)
+WEIBO_TEST_PASSWORD: str = env.str('WEIBO_PASSWORD', default=None)
 
 DB_DSN: str = env.str('DB_DSN', default='postgres://postgres:postgres@127.0.0.1:5400/postgres')
 
@@ -45,8 +45,8 @@ ANTI_CAPTCHA_KEY = env.str('ANTI_CAPTCHA_KEY', default='')
 UPDATE_POST_SECONDS = env.int('UPDATE_POST_SECONDS', default=10)
 SEND_POST_SECONDS = env.int('SEND_POST_SECONDS', default=10)
 
-WEIBO_HOST_URL = env.str('WEIBO_HOST_URL')
-WEIBO_NICK = env.str('WEIBO_NICK')
+WEIBO_HOST_URL = env.str('WEIBO_HOST_URL', default=None)
+WEIBO_NICK = env.str('WEIBO_NICK', default=None)
 
 TG_PROXY_URL = env.str('TG_PROXY_URL', default=None)
 
