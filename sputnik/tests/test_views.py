836beed45ea0c0c9e6da705fe8183d36eec33015
skar404 <skar404@gmail.com>
2019-05-03 01:52:57 +0300

    #2 update test

--- sputnik/tests/test_views.py
+++ sputnik/tests/test_views.py
@@ -1,9 +1,6 @@
 from sputnik.main import *
 
-from conftest import create_app
 
-
-async def test_client(test_client):
-    client = await test_client(create_app)
-    resp = await client.get('/ping')
-    assert resp.status == 200
+async def test_client(cli):
+    resp = await cli.get('/ping')
+    breakpoint()
