0d50d9c18ccef90f8e68bba2e0a7823867286d36
skar404 <skar404@gmail.com>
2019-05-02 17:24:48 +0300

    #2 add test test

--- sputnik/utils/telegram.py
+++ sputnik/utils/telegram.py
@@ -1,5 +1,5 @@
 from sputnik.settings import BOT_SECRET_CALLBACK
 
 
-def get_secret_callback(callback_name):
+def get_secret_callback(callback_name: str) -> str:
     return f'secret:{BOT_SECRET_CALLBACK}:{callback_name}'
