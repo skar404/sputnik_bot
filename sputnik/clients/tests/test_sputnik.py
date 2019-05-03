ac0650483990aed1c5b56fc4e3ba94323ebfd5e0
skar404 <skar404@gmail.com>
2019-05-04 01:14:50 +0300

    add test to telegram client + sdk

--- sputnik/clients/tests/test_sputnik.py
+++ sputnik/clients/tests/test_sputnik.py
@@ -3,6 +3,6 @@ from asynctest import patch
 from sputnik.clients.base import RequestData
 
 
-@patch('sputnik.clients.base.BaseClient._request', return_value=RequestData())
-async def test_get_post_data():
-    pass
+# @patch('sputnik.clients.base.BaseClient._request', return_value=RequestData())
+# async def test_get_post_data():
+#     pass
