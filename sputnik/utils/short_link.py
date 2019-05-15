82347a70fa3495a5d74dcb9772b3cce41fba2f0a
skar404 <skar404@gmail.com>
2019-05-16 00:56:45 +0300

    fix

--- sputnik/utils/short_link.py
+++ sputnik/utils/short_link.py
@@ -1,8 +1,9 @@
 from sputnik.clients.bitly import BitlyClint
-from sputnik.clients.sputnik import SputnikService, SputnikClintError
 
 
 async def get_short_link(post_id: str, full_link: str):
+    from sputnik.clients.sputnik import SputnikService, SputnikClintError
+
     try:
         short_link = await SputnikService().get_short_link(post_id[8:])
     except SputnikClintError:
