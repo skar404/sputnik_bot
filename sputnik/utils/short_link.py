722652483fcfff18ae2048bb9337eb4f5671d48c
skar404 <skar404@gmail.com>
2019-05-16 20:36:40 +0300

    off short link in sputnik

--- sputnik/utils/short_link.py
+++ sputnik/utils/short_link.py
@@ -2,15 +2,19 @@ from sputnik.clients.bitly import BitlyClint
 
 
 async def get_short_link(post_id: str, full_link: str):
-    from sputnik.clients.sputnik import SputnikService, SputnikClintError
+    # from sputnik.clients.sputnik import SputnikService, SputnikClintError
 
     short_link = None
-    try:
-        short_link = await SputnikService().get_short_link(post_id[8:])
-    except SputnikClintError:
-        short_link_bitly = await BitlyClint().create_short_link(full_link)
-        if short_link_bitly:
-            short_link = short_link_bitly.get('url') if short_link_bitly.get('url') else None
+    # try:
+    #     short_link = await SputnikService().get_short_link(post_id[8:])
+    # except SputnikClintError:
+    #     short_link_bitly = await BitlyClint().create_short_link(full_link)
+    #     if short_link_bitly:
+    #         short_link = short_link_bitly.get('url') if short_link_bitly.get('url') else None
+
+    short_link_bitly = await BitlyClint().create_short_link(full_link)
+    if short_link_bitly:
+        short_link = short_link_bitly.get('url') if short_link_bitly.get('url') else None
 
     if not short_link:
         short_link = full_link
