e32f5dab07fcaee080e94d6a52b2341b3ca4d539
skar404 <skar404@gmail.com>
2020-02-17 03:46:58 +0300

    update weibo client

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"sputnik_bok/app/client/weibo"
+)
+
+func main() {
+	login := os.Getenv("LOGIN")
+	pass := os.Getenv("PASS")
+
+	weibo.NewConfig().Login(login, pass)
 }
